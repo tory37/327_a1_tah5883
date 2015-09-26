cced8da8021c8d1776af03cbf7c7b9ea0f838a7a
tory37 <tory37@gmail.com>
2015-09-25 19:43:32 -0500

    Made boids

--- Assets/Scripts/Boid.cs
+++ Assets/Scripts/Boid.cs
@@ -5,6 +5,18 @@ using System.Collections.Generic;
 
 public class Boid : MonoBehaviour {
 
+	public enum BoidType
+	{
+		Green,
+		Blue,
+		Red,
+		Black
+	}
+
+	public BoidType type;
+
+	public bool canCollide = false;
+
 	private Vector3 v1 = Vector3.zero;
 	private Vector3 v2 = Vector3.zero;
 	private Vector3 v3 = Vector3.zero;
@@ -12,7 +24,15 @@ public class Boid : MonoBehaviour {
 
 	void Update () {
 
-		Vector3 baseVelocity = ( BoidManager.Player.transform.position - transform.position ).normalized * BoidManager.FlightSpeed;
+		//CheckCollision();
+		Vector3 vTotal = Cohesion() + Seperation() + Alignment();
+		float vMagnitude = vTotal.magnitude;
+		transform.forward = Vector3.RotateTowards(transform.forward, vTotal, BoidManager.RotationSpeed * Time.deltaTime, 0f);
+		transform.Translate( transform.forward * vMagnitude * Time.deltaTime);
+		transform.Translate( -transform.position.normalized * BoidManager.FlightSpeed * Time.deltaTime);
+		//transform.Translate( (Cohesion() + Seperation() + Alignment()) * Time.deltaTime);
+
+		/*Vector3 baseVelocity = ( BoidManager.Player.transform.position - transform.position ).normalized * BoidManager.FlightSpeed;
 
 		Collider[] nearbyBoids = Physics.OverlapSphere( transform.position, BoidManager.FlockMateDistance, BoidManager.BoidMask );
 
@@ -73,7 +93,211 @@ public class Boid : MonoBehaviour {
 		Velocity = Velocity + (Velocity * -BoidManager.DragForce * Time.deltaTime);
 		transform.Translate(Velocity * Time.deltaTime);
 
-		transform.LookAt( BoidManager.Player );
+		transform.LookAt( BoidManager.Player );*/
+	}
+
+	private void CheckCollision()
+	{
+		foreach ( Collider coll in Physics.OverlapSphere( transform.position, .3F, BoidManager.BoidMask ) )
+		{
+			if ( coll.GetComponent<Boid>() != null )
+			{
+				Interaction( coll.GetComponent<Boid>() );
+				return;
+			}
+		}
+	}
+
+	private Vector3 Cohesion()
+	{
+		Vector3 centerOfMass = transform.position;
+		int numberOfBoids = 0;
+		foreach ( Collider coll in Physics.OverlapSphere( transform.position, BoidManager.CohesionDistance, BoidManager.BoidMask ) )
+		{
+			Boid boid = coll.GetComponent<Boid>();
+			if (boid != null)
+			{
+				centerOfMass += boid.transform.position;
+				numberOfBoids++;
+			}
+		}
+		if ( numberOfBoids > 0 )
+			centerOfMass = centerOfMass / numberOfBoids;
+
+		//Debug.Log( "Cohesion: " + centerOfMass.normalized * BoidManager.CohesionStrength );
+		return ( centerOfMass.normalized - transform.position) * BoidManager.CohesionStrength;
+	}
+
+	private Vector3 Seperation()
+	{
+		Vector3 sepDirection = Vector3.zero;
+		int numberOfBoids = 0;
+		foreach ( Collider coll in Physics.OverlapSphere( transform.position, BoidManager.SeperationDistance, BoidManager.BoidMask ) )
+		{
+			Boid boid = coll.GetComponent<Boid>();
+			if (boid != null)
+			{
+				sepDirection += transform.position - boid.transform.position;
+				numberOfBoids++;
+			}
+		}
+		//Debug.Log("Seperation: " + sepDirection.normalized * BoidManager.SeperationStrength);
+		return sepDirection.normalized * BoidManager.SeperationStrength;
+	}
+
+	private Vector3 Alignment()
+	{
+		Vector3 newVelocity = Vector3.zero;
+		int numberOfBoids = 0;
+		foreach ( Collider coll in Physics.OverlapSphere( transform.position, BoidManager.AlignmentDistance, BoidManager.BoidMask ) )
+		{
+			Boid boid = coll.GetComponent<Boid>();
+			if (boid != null)
+			{
+				newVelocity += boid.Velocity;
+				numberOfBoids++;
+			}
+		}
+		//Debug.Log("Alignment: " + newVelocity / BoidManager.AlignmentWeakness);
+		return newVelocity / BoidManager.AlignmentWeakness;
+	}
+
+	void Interaction(Boid other)
+	{
+		Debug.Log( "Interaction" );
+		if ( canCollide && other.canCollide)
+		{
+			canCollide = false;
+			other.canCollide = false;
+			StartCoroutine( WaitToHitAgain() );
+
+			if ( other != null )
+			{
+				switch ( type )
+				{
+					case BoidType.Black:
+						switch ( other.type )
+						{
+							case BoidType.Black:
+								Destroy( gameObject );
+								Destroy( other.gameObject );
+								break;
+							case BoidType.Blue:
+								Destroy( other.gameObject );
+								break;
+							case BoidType.Green:
+								Destroy( other.gameObject );
+								break;
+							case BoidType.Red:
+								Destroy( other.gameObject );
+								break;
+						}
+						break;
+
+					case BoidType.Blue:
+						switch ( other.type )
+						{
+							case BoidType.Black:
+								Destroy( this.gameObject );
+								break;
+							case BoidType.Blue:
+								switch ( Random.Range( 1, 4 ) )
+								{
+									case 1:
+										Instantiate( BoidManager.BlackBoid );
+										break;
+									case 2:
+										Instantiate( BoidManager.BlackBoid );
+										break;
+									case 3:
+										Instantiate( BoidManager.GreenBoid );
+										break;
+									case 4:
+										Instantiate( BoidManager.RedBoid );
+										break;
+									default:
+										Instantiate( BoidManager.BlackBoid );
+										break;
+								}
+								break;
+							case BoidType.Green:
+								Instantiate( BoidManager.GreenBoid );
+								break;
+							case BoidType.Red:
+								Destroy( other.gameObject );
+								Instantiate( BoidManager.RedBoid );
+								break;
+						}
+						break;
+
+					case BoidType.Green:
+						switch ( other.type )
+						{
+							case BoidType.Black:
+								Destroy( this.gameObject );
+								break;
+							case BoidType.Blue:
+								Instantiate( BoidManager.GreenBoid );
+								break;
+							case BoidType.Green:
+								Instantiate( BoidManager.GreenBoid );
+								Instantiate( BoidManager.BlueBoid );
+								break;
+							case BoidType.Red:
+								Destroy( other.gameObject );
+								Instantiate( BoidManager.RedBoid );
+								break;
+						}
+						break;
+
+					case BoidType.Red:
+						switch ( other.type )
+						{
+							case BoidType.Black:
+								Destroy( this.gameObject );
+								break;
+							case BoidType.Blue:
+								Destroy( other.gameObject );
+								Instantiate( BoidManager.RedBoid );
+								break;
+							case BoidType.Green:
+								Destroy( other.gameObject );
+								Instantiate( BoidManager.RedBoid );
+								break;
+							case BoidType.Red:
+								Instantiate( BoidManager.BlackBoid );
+								Instantiate( BoidManager.BlackBoid );
+								break;
+						}
+						break;
+
+				}
+			}
+		}
+	}
+
+	IEnumerator WaitToHitAgain ()
+	{
+		switch (type)
+		{
+			case BoidType.Black:
+				yield return new WaitForSeconds( BoidManager.BlackWait );
+				break;
+			case BoidType.Blue:
+				yield return new WaitForSeconds( BoidManager.BlueWait );
+				break;
+			case BoidType.Green:
+				yield return new WaitForSeconds( BoidManager.GreenWait );
+				break;
+			case BoidType.Red:
+				yield return new WaitForSeconds( BoidManager.RedWait );
+				break;
+			default:
+				yield return new WaitForSeconds( 30f );
+				break;
+		}
+
+		canCollide = true;
 	}
 
 }
--- Assets/Scripts/BoidManager.cs
+++ Assets/Scripts/BoidManager.cs
@@ -3,24 +3,29 @@ using System.Collections;
 
 public class BoidManager : MonoBehaviour {
 
-	public Transform boidsContainer;
+	public static bool UseInteractions { get { return Instance.useInteractions; } }
+	public bool useInteractions;
 
+	public static float SeperationDistance {get {return Instance.seperationDistance;} }
+	[Header("Seperation")]
+	public float seperationDistance;
 	public static float SeperationStrength {get {return Instance.seperationStrength;} }
 	public float seperationStrength;
-	public static float SeperationDistance {get {return Instance.seperationDistance;} }
-	public float seperationDistance;
 
 	public static float AlignmentDistance { get { return Instance.alignmentDistance; } }
+	[Header("Alignment")]
 	public float alignmentDistance;
-	public static float AlignmentStrength {get {return Instance.alignmentStrength;} }
-	public float alignmentStrength;
+	public static float AlignmentWeakness {get {return Instance.alignmentWeakness;} }
+	public float alignmentWeakness;
 
 	public static float CohesionDistance {get {return Instance.cohesionDistance;} }
+	[Header("Cohesion")]
 	public float cohesionDistance;
 	public static float CohesionStrength {get {return Instance.cohesionStrength;} }
 	public float cohesionStrength;
 
 	public static float FlightSpeed {get {return Instance.flightSpeed;} }
+	[Header("Base Flight")]
 	public float flightSpeed;
 
 	public static float FlockMateAngle {get {return Instance.flockMateAngle;} }
@@ -29,7 +34,8 @@ public class BoidManager : MonoBehaviour {
 	public static float FlockMateDistance {get {return Instance.flockMateDistance;} }
 	public float flockMateDistance;
 
-	public static BoidManager Instance;
+	public static float RotationSpeed {get {return Instance.rotationSpeed;} }
+	public float rotationSpeed;
 
 	public static Transform Player {get {return Instance.player;} }
 	public Transform player;
@@ -46,12 +52,64 @@ public class BoidManager : MonoBehaviour {
 	public Boid boidPrefab;
 	public float spawnRadius;
 
+	public static BoidManager Instance;
+
+	public static Boid GreenBoid { get { return Instance.greenBoid; } }
+	[Header( "Green Boid" )]
+	public Boid greenBoid;
+	public float greenNum;
+	public static float GreenWait { get { return Instance.greenWait; } }
+	public float greenWait;
+
+	public static Boid RedBoid { get { return Instance.redBoid; } }
+	[Header("Red Boid")]
+	public Boid redBoid;
+	public float redNum;
+	public static float RedWait { get { return Instance.redWait; } }
+	public float redWait;
+
+	public static Boid BlueBoid { get { return Instance.blueBoid; } }
+	[Header( "Blue Boid" )]
+	public Boid blueBoid;
+	public float blueNum;
+	public static float BlueWait { get { return Instance.blueWait; } }
+	public float blueWait;
+
+	public static Boid BlackBoid { get { return Instance.blackBoid; } }
+	[Header( "Black Boid" )]
+	public Boid blackBoid;
+	public float blackNum;
+	public static float BlackWait { get { return Instance.blackWait; } }
+	public float blackWait;
+
 	void Start()
 	{
 		Instance = this;
-		for (int i = 0; i < numOfBoids; i ++)
+		if ( !useInteractions )
 		{
-			GameObject.Instantiate(boidPrefab, Random.insideUnitSphere * spawnRadius, Quaternion.identity);
+			for ( int i = 0; i < numOfBoids; i++ )
+			{
+				GameObject.Instantiate( boidPrefab, Random.insideUnitSphere * spawnRadius, Quaternion.identity );
+			}
+		}
+		else
+		{
+			for ( int i = 0; i < blueNum; i++ )
+			{
+				GameObject.Instantiate( blueBoid, Random.insideUnitSphere * spawnRadius, Quaternion.identity );
+			}
+			for ( int i = 0; i < blackNum; i++ )
+			{
+				GameObject.Instantiate( blackBoid, Random.insideUnitSphere * spawnRadius, Quaternion.identity );
+			}
+			for ( int i = 0; i < greenNum; i++ )
+			{
+				GameObject.Instantiate( greenBoid, Random.insideUnitSphere * spawnRadius, Quaternion.identity );
+			}
+			for ( int i = 0; i < redNum; i++ )
+			{
+				GameObject.Instantiate( redBoid, Random.insideUnitSphere * spawnRadius, Quaternion.identity );
+			}
 		}
 	}
 
