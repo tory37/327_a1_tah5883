b2b621d386ff7b009f5c57134338bfd336c46b59
tory37 <tory37@gmail.com>
2015-09-28 12:20:08 -0500

    Pushing interpose stuf

---
+++ Assets/Scripts/InterposeManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InterposeManager : MonoBehaviour {
+
+	[SerializeField] private Transform leftPlayer;
+	[SerializeField] private Transform rightPlayer;
+	[SerializeField] private Transform interposer;
+
+	[SerializeField] private float playerSpeed;
+
+	private void Update()
+	{
+		if ( Input.GetKey(KeyCode.W) )
+			leftPlayer.transform.Translate(Vector3.forward * playerSpeed * Time.deltaTime);
+		if ( Input.GetKey(KeyCode.S) )
+			leftPlayer.transform.Translate(Vector3.forward * -playerSpeed * Time.deltaTime);
+		if ( Input.GetKey(KeyCode.A) )
+			leftPlayer.transform.Translate(Vector3.right * playerSpeed * Time.deltaTime);
+		if ( Input.GetKey(KeyCode.D) )
+			leftPlayer.transform.Translate(Vector3.right * -playerSpeed * Time.deltaTime);
+
+		if ( Input.GetKey(KeyCode.UpArrow) )
+			rightPlayer.transform.Translate(Vector3.forward * playerSpeed * Time.deltaTime);
+		if ( Input.GetKey(KeyCode.DownArrow) )
+			rightPlayer.transform.Translate(Vector3.forward * -playerSpeed * Time.deltaTime);
+		if ( Input.GetKey(KeyCode.LeftArrow) )
+			rightPlayer.transform.Translate(Vector3.right * playerSpeed * Time.deltaTime);
+		if ( Input.GetKey(KeyCode.RightArrow) )
+			rightPlayer.transform.Translate(Vector3.right * -playerSpeed * Time.deltaTime);
+	}
+}
